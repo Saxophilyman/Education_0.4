32062c703856bd6f5bce5ae9b8f1d9636abfd698
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2023-08-03 10:19:55 +0400

    Update 04_SortingAndAggregation_01.sql

--- 04_SortingAndAggregation_01.sql
+++ 04_SortingAndAggregation_01.sql
@@ -1,7 +1,16 @@
 5.4.1. Отсортируйте список сотрудников Employees по дате рождения, начиная с самых молодых, и по стране
+       SELECT * FROM Employees
+       ORDER BY BirthDate DESC, Country;
 
 5.4.2. Исключите из предыдущего набора те записи, в которых поле Region равно NULL
+       SELECT * FROM Employees
+       WHERE Region IS NOT NULL
+       ORDER BY BirthDate DESC, Country;
 
 5.4.3. Найдите среднюю, минимальную и максимальную цены по полю UnitPrice из таблицы Order Details.
+       SELECT MIN(UnitPrice) FROM [Order Details];
+       SELECT AVG(UnitPrice) FROM [Order Details];
+       SELECT MAX(UnitPrice) FROM [Order Details];
 
 5.4.4. Подсчитайте количество уникальных городов в списке пользователей (Customers).
+       SELECT COUNT (DISTINCT City) FROM Customers;
