cc3b52376d963813b1c69a0ce24d41525c60c691
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2023-08-05 12:49:40 +0400

    Update 07_TableConnections_01.sql

--- 07_TableConnections_01.sql
+++ 07_TableConnections_01.sql
@@ -1,1 +1,8 @@
+8.3.1. Сформируйте список названий товаров (таблица Products) с указанием для каждого из них соответствующей категории (таблица Categories).
+       SELECT Products.ProductName, Categories.CategoryName
+       FROM Products, Categories
+       WHERE Products.CategoryID = Categories.CategoryID;
+  
+8.3.2. Организуйте эквисоединение, которое выводит цену и названия тех товаров, для которых цена за единицу (UnitPrice) в таблице Order Details меньше 20.
 
+8.3.3. Добавьте к предыдущему запросу третью таблицу Categories, и выведите в дополнение к названию товара его категорию.
