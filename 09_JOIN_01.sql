e6615513e0a6e1460b09952161ff994905e3875e
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2023-08-14 17:48:46 +0400

    Update 09_JOIN_01.sql

--- 09_JOIN_01.sql
+++ 09_JOIN_01.sql
@@ -1,7 +1,11 @@
 10.4.1. Перепишите задание 8.3.2 через синтаксис JOIN.
 8.3.2 Организуйте эквисоединение, которое выводит цену и названия тех товаров, для которых цена за единицу (UnitPrice) в таблице Order Details меньше 20.
-
   
+        SELECT  Products.ProductName, [Order Details].UnitPrice
+        FROM Products JOIN [Order Details]
+        ON  [Order Details].ProductID = Products.ProductID AND [Order Details].UnitPrice < 20;
+
+
   
 10.4.2. Имеется запрос
 SELECT Orders.Freight, Customers.CompanyName
@@ -10,6 +14,9 @@ ON Orders.CustomerID = Customers.CustomerID
 ORDER BY Freight;
 Проверьте этот запрос с вариантом FULL JOIN -- за счёт чего выдача получилась объёмнее? Почему значения NULL встречаются в обоих полях набора?
 
+        Насколько понимаю выдача получается за тсчёт разницы обработки запросов INNER и FULL JOIN.
+        Так как INNER JOIN объединяет таблицы исходя из наличия данных и соответствия в первой и второй таблице.
+        А FULL JOIN объединяет данные и без наличия соответствия данных из противоположной таблице, задавая им значение NULL.
   
   
 10.4.3. Подумайте, как с помощью предложения WHERE превратить запрос CROSS JOIN в INNER JOIN.
