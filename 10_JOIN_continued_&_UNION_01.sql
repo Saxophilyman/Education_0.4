9ac383de45552f4b8a4944bae988dd8b5737607a
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2023-08-18 06:26:36 +0400

    Update 10_JOIN_continued_&_UNION_01.sql

--- 10_JOIN_continued_&_UNION_01.sql
+++ 10_JOIN_continued_&_UNION_01.sql
@@ -1,9 +1,10 @@
 11.5.1. Отберите с помощью LEFT JOIN все записи из таблицы Customers, для которых FK-ключ таблицы Orders пустой.
-
-
-
-
-  
+        SELECT * FROM Customers
+        LEFT JOIN Orders ON Customers.CustomerID = Orders.CustomerID
+        WHERE Orders.CustomerID IS NULL;  
 
 11.5.2. Выведите конкретную информацию по всем пользователям Customers и поставщикам Suppliers -
         имя контактной персоны, город и страну, а также идентификацию группы (пользователь или поставщик).
+        SELECT 'Customer' As Type, ContactName, City, Country FROM Customers
+        UNION
+        SELECT 'Supplier' As Type, ContactName, City, Country FROM Suppliers;
