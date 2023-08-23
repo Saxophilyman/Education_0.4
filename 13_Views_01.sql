d4c9f8131932a58184bed2e66da3975e1dc7fe39
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2023-08-23 19:28:16 +0400

    Create 13_Views_01.sql

---
+++ 13_Views_01.sql
@@ -0,0 +1,9 @@
+14.1. Чем представление Invoices отличается от таблицы Orders?
+
+14.2. Что делает представление "CategorySales for 1997"?
+
+14.3. Что делает представление "Sales Total by Amount"?
+
+14.4. Что делает представление "Products Above Average Price"?
+
+14.5. Что делает представление "Summary of Sales by Quarter"?
