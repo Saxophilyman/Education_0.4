9f1635de3347e0549c65f26c3832a53a8a15b7f1
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2023-08-29 07:07:12 +0400

    Update 15_Keys_&_Indexes_01.sql

--- 15_Keys_&_Indexes_01.sql
+++ 15_Keys_&_Indexes_01.sql
@@ -1,2 +1,7 @@
 Добавьте подходящие индексы для таблиц тестовой базы, созданной в предыдущем занятии.
 
+CREATE  CLUSTERED INDEX PK_Region ON Region (RegionID);
+
+CREATE  CLUSTERED INDEX PK_Territories ON Territories (TerritoryID);
+
+
