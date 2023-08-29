ccb7af99ea0d00528471a5b31c1fd019c6318164
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2023-08-29 06:37:29 +0400

    Update 14_WorkingWithDatabases_01.sql

--- 14_WorkingWithDatabases_01.sql
+++ 14_WorkingWithDatabases_01.sql
@@ -6,5 +6,13 @@
 	 RegionID int NOT NULL); 
 
 2. Добавьте в неё и таблицу Region несколько значений так, чтобы они оказались связаны друг с другом через FK.
+   INSERT INTO Region (RegionID, RegionDescription)
+   VALUES (1, 'Central'), (2, 'Northwest'), (3, 'South');
 
+   INSERT INTO Territories (TerritoryID,TerritoryDescription, RegionID)
+   VALUES (1, 'Belgorodskaya', 1), (2, 'Bryanskaya', 1), (3, 'Vladimirskaya', 1),
+   (4, 'Arhangelskaya', 2), (5, 'Vologodskaya', 2), (6, 'Kaliningradskaya', 2),
+   (7, 'Adugeya', 3), (8, 'Astrahanskaya', 3), (9, 'Volgogradskaya', 3);
+
+Насколько понимаю непосредственную связь между ключами нужно задавать отдельно. Но по аналогии должно выглядеть вышеописанным образом.
    
